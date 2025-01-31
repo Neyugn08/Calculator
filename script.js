d418894f158ee7e19b10c86c96fbfdac99fd08e0
Neyugn <vukhangnguyen08@gmail.com>
2025-01-31 14:05:59 +0700

    Addding the slide and updating the screen

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-// Modifying the size of the keys 
-let keys = document.querySelector(".keys");
-let operator = keys.children;
+// Setting up the screen
+const screen = document.querySelector(".screen");
+let scrOriH = getComputedStyle(screen).height;
+screen.textContent = " ";
+const base = document.querySelector(".base");
+let operation;
+
+// Setting up the keys
+const keys = document.querySelector(".keys");
+const operator = keys.children;
 Array.from(operator).forEach(item => {
+    // Modifying the size of the keys
     item.style.width = 75/4 + "px";
     item.style.height = "15.7px";
+    // Making the keys function
+    item.addEventListener("click", (e) => {
+        // Populating the screen
+        screen.textContent = screen.textContent + e.target.textContent;
+        operation = screen.textContent;
+    });
 });
+
+// 4 basic operation 
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function mutiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
