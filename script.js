36b84e5e457629f07f400a7e8265a1071931c33c
Neyugn <vukhangnguyen08@gmail.com>
2025-01-31 15:36:55 +0700

    Adding checking for invalid usages features

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // Setting up the screen
 const screen = document.querySelector(".screen");
 let scrOriH = getComputedStyle(screen).height;
-//screen.textContent = " ";
-
 const container = document.querySelector(".container");
 container.textContent = " ";
-
 const base = document.querySelector(".base");
 let operation;
+let counterSym = 0;
 
 // Setting up the keys
 const keys = document.querySelector(".keys");
@@ -18,10 +16,24 @@ Array.from(operator).forEach(item => {
     item.style.height = "15.7px";
     // Making the keys function
     item.addEventListener("click", (e) => {
+        // Checking for invalid usage 
+        if (Number.isNaN(parseInt(e.target.textContent))) {
+            if (container.textContent === " ") return;
+            else {
+                counterSym++;
+            }
+            if (counterSym == 2) {
+                counterSym = 1;
+                return;
+            }
+        }
+        else {
+            counterSym = 0;
+        }
+
         // Populating the screen
-        /*screen.textContent = screen.textContent + e.target.textContent;
-        operation = screen.textContent;*/
         container.textContent = container.textContent + e.target.textContent;
+        operation = container.textContent;
 
         // Handling the overflow 
         if (parseFloat(getComputedStyle(container).width.replace("px", "")) >= 75) {
@@ -31,6 +43,11 @@ Array.from(operator).forEach(item => {
             expand16(screen);
             expand16(container);
             container.textContent = container.textContent + e.target.textContent;
+        }
+
+        // Handling the operation 
+        if (e.target.textContent == "=") {
+            calculating(operation);
         }
     });
 });
@@ -55,4 +72,19 @@ function divide(a, b) {
 // Expanding height by 16px function
 function expand16(div) {
     div.style.height = parseInt(getComputedStyle(div).height.replace("px", "")) + 16 + "px";
+}
+
+function calculating(operation) {
+    // Filtering the operation
+    operation = operation.replace(/\n/g, "");
+    if (operation[0] === " " && operation.length != 0) {
+        // Filtering out the equal sign at the end
+        operation = operation.slice(1, operation.length - 1);
+    }
+
+    // Checking for invalid usage
+    if (parseInt(operation.length) < 3) {
+        container.textContent = container.textContent.slice(0, container.textContent.length - 1);
+        return;
+    }
 }
